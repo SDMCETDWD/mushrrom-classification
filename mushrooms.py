901c595511dbbbaa40c8c3550de653aa68bbfac6
bhargavi naik <31363874+SDMCETDWD@users.noreply.github.com>
2018-12-09 19:32:11 +0530

    Update mushrooms.py

--- mushrooms.py
+++ mushrooms.py
@@ -1,20 +1,25 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sun Dec  9 14:07:27 2018
-
-@author: HP-PC
-"""
-
+#import all the necessary libraries
 import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import mean_squared_error 
 
-data = pd.read_csv(r'C:\Users\HP-PC\Desktop\data_science\ML_Exrcise\datasets\mushrooms.csv')
+# Load the dataset
+data = pd.read_csv(r'path-to-the-directory\mushrooms.csv')
+# Print the starting 5 samples
 print(data.head())
 
+# The dataset contain the non-numerical data. covert the data to the format supported by the algorithms 
+# This data covertion method is called data wrangling
+# For the features having 2 different entries the we will map one entry to 1 and the other one to 0
+# For the features having more than 2 types of entries we create more columns equal to the number of entries with the prefix being...
+# ... old column name and the entries as 1 or 0 according to the type
+# Ex: for a column X with types a,b,c the column will be split as X_a,X_b,X_c and X_will be 1 for the wherever the entries in the old..
+# column was a and rest entries in the X_a will be 0 same for X_b and X_c
+
 def wrangle(data):
+
     data['bruises'] = data['bruises'].map({'t': 1, 'f':0}).astype(int)
     
     cap_shape_sep = pd.get_dummies(data['cap-shape'],prefix = 'cap-shape')
@@ -88,7 +93,8 @@ def wrangle(data):
     habitat_sep = pd.get_dummies(data['habitat'],prefix = 'habitat')
     data = pd.concat([data,habitat_sep],axis=1)
     data = data.drop('habitat',axis=1)
-    
+
+# class column contains the labels(edible or poisonous) so we seperate the column
     data['class'] = data['class'].map({'e': 1, 'p':0}).astype(int)
     data = data.drop('veil-type',axis=1)
     return data
@@ -100,25 +106,40 @@ print(data.head())
 Y = data['class']
 X = data.drop('class',axis=1)
 
+# create the validation set from the given data
 X_train, X_val, y_train, y_val = train_test_split(X, Y, 
                                                     test_size=0.1, 
                                                     random_state=123, 
                                                     stratify=Y)
 
+# split the remaining data into test and training dataset
 X_train, X_test, y_train, y_test = train_test_split(X_train, y_train, 
                                                     test_size=0.1, 
                                                     random_state=123, 
                                                     stratify=y_train)
 
+# Choose a classifer model
 clf = RandomForestClassifier()
+
+# make sure all the features are in the right forma
 print(X_train.dtypes)
+
+# train the selected classifier with training data set
 clf.fit(X_train,y_train)
+
+# evaluate the classifier on training and validation set
 train_acc = round(clf.score(X_train,y_train),2)
 val_acc = round(clf.score(X_val,y_val),2)
 print('training_acuracy:',train_acc)
 print('val_accuracy:',val_acc)
+
+# check the decision making ability of the model on unseen test data
 y_pred = clf.predict(X_test)
+
+# print the starting 10 samples of predicted and actual y test
 print('predicted',y_pred[:10])
 print('y_test',y_test[:10])
+
+# check how the actual and predicted values differ from each other
 err = mean_squared_error(y_pred,y_test)
 print('error:',err)
